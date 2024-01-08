accb79c11281021a7ff6aca7dfc8d1f4e84cbab6
amansagar0403 <as624@snu.edu.in>
2024-01-08 23:13:19 +0530

    css,eventListener

---
+++ script.js
@@ -0,0 +1,7 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.querySelector("#input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button Clicked!!")
+})
