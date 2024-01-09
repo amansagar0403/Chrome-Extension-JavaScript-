1b1c688ea45a2ccdafba44242bb36d044b4cd9ce
amansagar0403 <as624@snu.edu.in>
2024-01-09 18:24:34 +0530

    icon added

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ let ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value = "";
+    renderLeads();
 })
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+        // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a><li>"
+        listItems += `
+            <li>
+                <a href='${myLeads[i]}' target='_blank'>
+                    ${myLeads[i]} 
+                </a>
+            <li>`
+        //template strings
 
-for(let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li);
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li);
+    }
+    ulEl.innerHTML = listItems
 }
