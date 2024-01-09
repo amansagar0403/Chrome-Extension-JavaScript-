3b1fc4ebf79f39b21180c4a6a070a8da2fdb5402
amansagar0403 <as624@snu.edu.in>
2024-01-09 17:41:38 +0530

    list done

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.querySelector("#input-btn")
+let ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button Clicked!!")
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li);
+}
